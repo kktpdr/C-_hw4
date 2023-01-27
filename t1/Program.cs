3bf16e638848c009c9305d1a700568a6b895ae60
Mikhail <goitw@outlook.com>
2023-01-27 18:48:37 +0100

    t1, t2

---
+++ t1/Program.cs
@@ -0,0 +1,21 @@
+﻿int Promt(string msg)
+{
+    System.Console.WriteLine($"{msg}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int a = Promt("Put A");
+int b = Promt("Put B");
+
+int Exponentiation(int a, int b){
+    int result = 1;
+    for (int i=1; i<b+1; i++)
+    {
+        result = result*a;
+    }
+
+    return result;
+}
+int exponentiation = Exponentiation(a, b);
+System.Console.WriteLine($"{exponentiation}");
---
+++ t2/Program.cs
@@ -0,0 +1,24 @@
+﻿int Promt(string msg)
+{
+    System.Console.WriteLine($"{msg}: ");
+    return int.Parse(Console.ReadLine());
+}
+
+
+int num = Promt("Put a number");
+int SumNumber(int numberN){
+int counter = Convert.ToString(num).Length;
+int advance = 0;
+int result = 0;
+
+for (int i = 0; i < counter; i++)
+{
+    advance = num - num % 10;
+    result = result + (num - advance);
+    num = num / 10;
+    }
+   return result;
+}
+
+int sumNumber = SumNumber(num);
+Console.WriteLine("Sum: " + sumNumber);
